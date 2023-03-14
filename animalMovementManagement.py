f92f3903bc432eef05fc354635ad67cdad97f90c
Mirieri David Mogaka <davemirieri@gmail.com>
2023-03-14 21:37:44 +0300

    resolve errors update

--- animalMovementManagement.py
+++ animalMovementManagement.py
@@ -1,10 +1,10 @@
-import mysql.connector  # to connect with MySQL server
-from dotenv import load_dotenv  # to load environment variables from .env file
-import os  # to handle environment variables and files
-import csv  # to read and write CSV files
-import smtplib  # to send email using SMTP protocol
-from email.mime.multipart import MIMEMultipart  # to create email message with attachment(s)
-from email.mime.text import MIMEText  # to add plain text in email message
+import mysql.connector
+from dotenv import load_dotenv
+import os
+import csv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
 # Load the .env file
@@ -66,7 +66,7 @@ def send_email_with_report():
         msg['To'] = 'd.mogaka@cgiar.org'  # Replace with recipient email address
         msg['Subject'] = 'Update to show Movement to new Farms'
 
-        body = 'Please find attached CSV showing animal movement this week.\n\n Best Regards,\nTeam'
+        body = 'Please find attached CSV showing animal movement this week.\n\nBest Regards,\nDavid'
 
         msg.attach(MIMEText(body, 'plain'))
 
@@ -77,7 +77,7 @@ def send_email_with_report():
 
         smtp = smtplib.SMTP('smtp.gmail.com', 587)  # Replace with your SMTP server and port details
         smtp.starttls()
-        smtp.login('noreply.adgg@gmail.com', '!2sYstemmaster')  # Replace with your own email credentials
+        smtp.login('noreply.adgg@gmail.com', 'rvpizpgabllbuwfo')  # Replace with your own email credentials
         smtp.sendmail(msg['From'], msg['To'], msg.as_string())
         smtp.quit()
 
@@ -100,9 +100,9 @@ def execute_update_query():
         UPDATE
           core_animal
         INNER JOIN core_animal_event ON core_animal.id = core_animal_event.animal_id
-        INNER JOIN core_farm ON REPLACE(IFNULL(JSON_EXTRACT(core_animal_event.additional_attributes, "$.@557"), ""), "'", "") = core_farm.id
+        INNER JOIN core_farm ON REPLACE(IFNULL(JSON_EXTRACT(core_animal_event.additional_attributes, '$."557"'), ''), '"', '') = core_farm.id
         SET
-          core_animal.farm_id = REPLACE(IFNULL(JSON_EXTRACT(core_animal_event.additional_attributes, "$.@557"), ""), "'", ""),
+          core_animal.farm_id = REPLACE(IFNULL(JSON_EXTRACT(core_animal_event.additional_attributes, '$."557"'), ''), '"', ''),
           core_animal.region_id = core_farm.region_id,
           core_animal.district_id = core_farm.district_id,
           core_animal.ward_id = core_farm.ward_id,
